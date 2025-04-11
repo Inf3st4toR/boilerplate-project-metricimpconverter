e1ebf27656b2e1ffe1c774dbed5e455ba0338852
inf3st4tor <xcharaix@gmail.com>
2025-04-11 03:13:14 +0000

    Completed Functional Testing

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -25,4 +25,68 @@ suite("Functional Tests", function () {
         done();
       });
   });
+
+  //Test_2
+  test("Convert 32g /error", (done) => {
+    chai
+      .request(server)
+      .get("/api/convert?input=32g")
+      .end((err, res) => {
+        assert.equal(res.status, 200, "Server Response error");
+        assert.equal(res.body, "invalid unit", "Error message invalid");
+        done();
+      });
+  });
+
+  //Test_3
+  test("Convert 3/7.2/4kg /error", (done) => {
+    const input = encodeURIComponent("3/7.2/4kg");
+    const url = `/api/convert?input=${input}`;
+    chai
+      .request(server)
+      .get(url)
+      .end((err, res) => {
+        assert.equal(res.status, 200, "Server Response error");
+        assert.equal(res.body, "invalid number", "Error message invalid");
+        done();
+      });
+  });
+
+  //Test_4
+  test("Convert Num&Unit /error", (done) => {
+    const input = encodeURIComponent("3/7.2/4kilomegagram");
+    const url = `/api/convert?input=${input}`;
+    chai
+      .request(server)
+      .get(url)
+      .end((err, res) => {
+        assert.equal(res.status, 200, "Server Response error");
+        assert.equal(
+          res.body,
+          "invalid number and unit",
+          "Error message invalid"
+        );
+        done();
+      });
+  });
+
+  //Test_5
+  test("Convert kg", (done) => {
+    chai
+      .request(server)
+      .get("/api/convert?input=kg")
+      .end((err, res) => {
+        assert.equal(res.status, 200, "Server Response error");
+        assert.equal(res.body.initNum, 1, "initNum error");
+        assert.strictEqual(res.body.initUnit, "kg", "initUnit error");
+        assert.approximately(
+          res.body.returnNum,
+          2.20462,
+          0.1,
+          "returnNum error"
+        );
+        assert.equal(res.body.returnUnit, "lbs", "returnUnit error");
+        done();
+      });
+  });
 });
